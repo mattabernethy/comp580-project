d3d92d240ef27de8c1f11c0b1225725dadcbdc8a
Michael Qi <qi.michael101@gmail.com>
2020-03-11 00:38:23 -0400

    Functional working randomly generated addition questions each with 1 correct answer choice and 2 randomly generated incorrect answer choices. Difficulty for easy medium hard are all the same for now

--- script.js
+++ script.js
@@ -1,5 +1,63 @@
 $(document).ready(function () {
-    $(".difficultyButton").click(function (event) {
+
+    // Input (nothing yet, maybe difficulty later?), Output (void: generates random addition question on screen)
+    function generateAdditionQuestion() {
+        const leftNum = Math.floor(Math.random() * 6) + 1;
+        const rightNum = Math.floor(Math.random() * 6) + 1;
+        const equation = leftNum + " + " + rightNum;
+        $("#question").find("p").text(equation);
+    }
+
+    // Input (nothing), Output (void: generates 1 correct answer and 2 random incorrect answers on screen)
+    function generateAnswerChoices() {
+        // Calculates correct answer of current displayed question
+        const correctAns = eval($("#question").find("p").text());
+
+        // Returns random integer 0, 1, or 2 to decide correctAns location
+        const correctAnsId = Math.floor(Math.random() * 3);     
+        // Returns either -1 or 1
+        const positiveOrNeg = Math.round(Math.random()) * 2 - 1;    
+
+        // New answer choices
+        if (correctAnsId == 0) {
+            $("#answerOne").find("span").text(correctAns);
+            if (positiveOrNeg == -1) {
+                // Then first non-correct choice is multiplied by -1
+                $("#answerTwo").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
+                $("#answerThree").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
+            } else {
+                // Else second non-correct choice is multiplied by -1
+                $("#answerTwo").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
+                $("#answerThree").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
+            }
+        } else if (correctAnsId == 1) {
+            $("#answerTwo").find("span").text(correctAns);
+            if (positiveOrNeg == -1) {
+                // Then first non-correct choice is multiplied by -1
+                $("#answerOne").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
+                $("#answerThree").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
+            } else {
+                // Else second non-correct choice is multiplied by -1
+                $("#answerOne").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
+                $("#answerThree").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
+            }
+        } else {
+            $("#answerThree").find("span").text(correctAns);
+            if (positiveOrNeg == -1) {
+                // Then first non-correct choice is multiplied by -1
+                $("#answerOne").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
+                $("#answerTwo").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
+            } else {
+                // Else second non-correct choice is multiplied by -1
+                $("#answerOne").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
+                $("#answerTwo").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
+            }
+        }
+    }
+
+
+    // Transitions to battle page upon clicking a difficulty on main menu
+    $(".difficultyButton").click(function () {
         console.log(this);
         const difficulty = $(this).find("p").text();
         console.log(difficulty);
@@ -20,12 +78,16 @@ $(document).ready(function () {
 
                     // Adjust difficulty based on difficulty button selected
                     if (difficulty == "Easy") {
-                        
+
                     } else if (difficulty == "Medium") {
 
                     } else {
 
                     }
+
+                    // Generate first question and its answer choices
+                    generateAdditionQuestion();
+                    generateAnswerChoices();
 
                     $("#battlePageWrapper").fadeIn(250);
                 })
@@ -33,7 +95,27 @@ $(document).ready(function () {
         })
 
 
-    })
+    });
+
+
+    $(document).on("click", ".answerButton", function () {
+        const correctAns = eval($("#question").find("p").text());
+        const yourAns = $(this).find("span").text();
+        console.log("Your Answer: " + yourAns);
+        console.log("Correct Answer: " + correctAns);
+
+        const isCorrect = (yourAns == correctAns);
+        // If correct, monster takes damage. If incorrect, you take damage.
+        if (isCorrect) {
+            console.log("Monster takes damage!");
+        } else {
+            console.log("You take damage.");
+        }
+
+        // New question and corresponding answer choices
+        generateAdditionQuestion();
+        generateAnswerChoices();
+    });
 
 
 
