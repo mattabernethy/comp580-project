24eaec7de84766eb303701042b9cc0ebfe63c2ab
Michael Qi <qi.michael101@gmail.com>
2020-03-13 23:10:25 -0400

    Added damage mechanics. Difficulty based off how much time you have for each question as of right now (can be changed later). Fixed addition answer choices rng

--- script.js
+++ script.js
@@ -2,11 +2,35 @@ $(document).ready(function () {
     let timer;  // Global question timer variable
     let difficulty;
     let streak = 0;
+    let playerHealth = 3;
+    let monsterHealth = 4;
+    let playerAttack = 1;
+
+    function monsterTakesDamage() {
+        streak += 1;
+
+        if (streak >= 4) {
+            playerAttack = 2;
+        } else {
+            playerAttack = 1;
+        }
+
+        monsterHealth -= playerAttack;
+        console.log("Monster takes damage!");
+        console.log("Monster health remaining: " + monsterHealth);
+    }
+
+    function playerTakesDamage() {
+        streak = 0;
+        playerHealth -= 1;
+        console.log("You take damage.");
+        console.log("Your health remaining: " + playerHealth);
+    }
 
     // Input (nothing yet, maybe difficulty later?), Output (void: generates random addition question on screen)
     function generateAdditionQuestion() {
-        const leftNum = Math.floor(Math.random() * 6) + 1;
-        const rightNum = Math.floor(Math.random() * 6) + 1;
+        const leftNum = Math.floor(Math.random() * 10) + 1;
+        const rightNum = Math.floor(Math.random() * 10) + 1;
         const equation = leftNum + " + " + rightNum;
         $("#question").find("p").text(equation);
     }
@@ -18,66 +42,67 @@ $(document).ready(function () {
 
         // Returns random integer 0, 1, or 2 to decide correctAns location
         const correctAnsId = Math.floor(Math.random() * 3);
-        // Returns either -1 or 1
-        const positiveOrNeg = Math.round(Math.random()) * 2 - 1;
 
-        // New answer choices
+        // Generate incorrect answers
+        const positiveOrNeg1 = Math.round(Math.random()) * 2 - 1;   // Returns either -1 or 1
+        const positiveOrNeg2 = Math.round(Math.random()) * 2 - 1;
+
+        const incorrectChoice1 = correctAns + (positiveOrNeg1 * (Math.floor(Math.random() * 5) + 1));
+        let incorrectChoice2 = correctAns + (positiveOrNeg2 * (Math.floor(Math.random() * 5) + 1));
+
+        // Makes sure there are no duplicate incorrect choices
+        while (incorrectChoice2 == incorrectChoice1) {
+            incorrectChoice2 = correctAns + (positiveOrNeg2 * (Math.floor(Math.random() * 5) + 1));
+        }
+
+        // Display new answer choices
         if (correctAnsId == 0) {
             $("#answerOne").find("span").text(correctAns);
-            if (positiveOrNeg == -1) {
-                // Then first non-correct choice is multiplied by -1
-                $("#answerTwo").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
-                $("#answerThree").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
-            } else {
-                // Else second non-correct choice is multiplied by -1
-                $("#answerTwo").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
-                $("#answerThree").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
-            }
+            $("#answerTwo").find("span").text(incorrectChoice1);
+            $("#answerThree").find("span").text(incorrectChoice2);
+
         } else if (correctAnsId == 1) {
             $("#answerTwo").find("span").text(correctAns);
-            if (positiveOrNeg == -1) {
-                // Then first non-correct choice is multiplied by -1
-                $("#answerOne").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
-                $("#answerThree").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
-            } else {
-                // Else second non-correct choice is multiplied by -1
-                $("#answerOne").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
-                $("#answerThree").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
-            }
+            $("#answerOne").find("span").text(incorrectChoice1);
+            $("#answerThree").find("span").text(incorrectChoice2);
+
         } else {
             $("#answerThree").find("span").text(correctAns);
-            if (positiveOrNeg == -1) {
-                // Then first non-correct choice is multiplied by -1
-                $("#answerOne").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
-                $("#answerTwo").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
-            } else {
-                // Else second non-correct choice is multiplied by -1
-                $("#answerOne").find("span").text(correctAns + Math.floor(Math.random() * 5) + 1);
-                $("#answerTwo").find("span").text(correctAns + (Math.floor(Math.random() * 5) + 1) * -1);
-            }
+            $("#answerOne").find("span").text(incorrectChoice1);
+            $("#answerTwo").find("span").text(incorrectChoice2);
         }
     }
 
 
-    // Input (nothing for now, maybe difficulty later?), Output (void: changes timer number on screen)
+    // Input (nothing for now), Output (void: sets time limit based on difficulty, starts question timer countdown on screen)
     function generateQuestionTimer() {
         let startTime = Date.now();
-        let timeLimit = 6;
+        let timeLimit;
+
+        // Adjust difficulty based on difficulty button selected
+        if (difficulty == "Easy") {
+            timeLimit = 30;
+        } else if (difficulty == "Medium") {
+            timeLimit = 15;
+        } else {
+            timeLimit = 5;
+        }
+
         let timeRemaining = timeLimit;
 
         $("#timerCountDown").text(timeRemaining);
         console.log(timeRemaining);
 
         timer = setInterval(function () {
-            timeRemaining = timeLimit -  Math.floor((Date.now() - startTime) / 1000);
+            timeRemaining = timeLimit - Math.floor((Date.now() - startTime) / 1000);
             $("#timerCountDown").text(timeRemaining);
             console.log(timeRemaining);
 
             // If time runs out, you take damage. Then generate next question.
             if (Math.floor(timeRemaining) <= 0) {
                 clearInterval(timer);
-                console.log("You take damage.");
-                streak = 0;
+
+                playerTakesDamage();
 
                 updateStreak();
                 generateAdditionQuestion();
@@ -93,9 +118,9 @@ $(document).ready(function () {
     }
 
 
-    // Updates the streak displayed on screen
+    // Updates player hit streak displayed on screen
     function updateStreak() {
-        if (streak >= 3) {
+        if (streak >= 4) {
             $("#comboNumber").text("Combo x" + streak + "!");
             $("#damageNumber").text("Damage x2!");
         } else {
@@ -126,15 +151,6 @@ $(document).ready(function () {
                     // Change id of frontPageWraper to battlePageWrapper
                     $("#frontPageWrapper").attr("id", "battlePageWrapper");
 
-                    // Adjust difficulty based on difficulty button selected
-                    if (difficulty == "Easy") {
-
-                    } else if (difficulty == "Medium") {
-
-                    } else {
-
-                    }
-
                     // Generate first question and its answer choices
                     updateStreak();
                     generateAdditionQuestion();
@@ -156,17 +172,14 @@ $(document).ready(function () {
 
         const correctAns = eval($("#question").find("p").text());
         const yourAns = $(this).find("span").text();
-        console.log("Your Answer: " + yourAns);
-        console.log("Correct Answer: " + correctAns);
+        console.log("Your Answer: " + yourAns + "    Correct Answer: " + correctAns);
 
         const isCorrect = (yourAns == correctAns);
         // If correct, monster takes damage. If incorrect, you take damage.
         if (isCorrect) {
-            console.log("Monster takes damage!");
-            streak += 1;
+            monsterTakesDamage();
         } else {
-            console.log("You take damage.");
-            streak = 0;
+            playerTakesDamage();
         }
 
         // New question and corresponding answer choices
